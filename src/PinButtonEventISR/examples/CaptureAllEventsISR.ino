e2074260424e4e4cac6207e6ab9754774f722418
diabolusss <hodiko.v@inbox.lv>
2021-12-29 18:39:03 +0000

    store baseline; upd docs

---
+++ src/PinButtonEventISR/examples/CaptureAllEventsISR.ino
@@ -0,0 +1,44 @@
+/*
+   Capture debounced button events 
+   This example code is in the public domain.
+*/
+
+#define DEBUG
+#include <PinButtonEventISR.h>
+
+PinButtonEventISR D7Button(7);//attach button leg to data pin
+
+void setup() {
+  // initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+void loop() {
+  Serial.print(".");
+  
+  if(D7Button.hasEvent()){
+    Serial.print("event ready: ");
+    
+    if(D7Button.isLongPress()){
+      Serial.println("LONG PRESS");
+      
+    }else if(D7Button.isPress()){
+      Serial.println("PRESS");
+      
+    }else if(D7Button.isClick()){
+      if(D7Button.isDoubleClick()){
+        Serial.println("DBL CLICK");
+        
+      }else if(D7Button.isSingleClick()){
+        Serial.println("SNGL CLICK");
+      }
+      
+    }
+    
+  }
+
+  delay(500);
+}
